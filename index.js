83efce2a194e6261aa912ec6c285c34d0d711193
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-09-25 22:41:19 -0400

    Completed the findMinAndRemoveSorted function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+const findMinAndRemoveSorted = (array) => {
+  let min = array[0];
+  let minIndex;
+  for (let i = 1; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
+}
