c55c6ee3ed654a146d99375065d9127821bf478d
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-10-05 20:16:02 -0400

    Completed the merge function

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ const findMinAndRemoveSorted = (array) => {
   array.splice(minIndex, 1);
   return min;
 }
+
+const findMinAndRemove = (firstHalf, secondHalf) => {
+  let minFirstHalf = firstHalf[0];
+  let minSecondHalf = secondHalf[0];
+  if (minFirstHalf < minSecondHalf) {
+    return firstHalf.shift();
+  } else {
+    return secondHalf.shift();
+  }
+}
+
+const merge = (firstHalf, secondHalf) => {
+  let sorted = [];
+  let currentMin;
+  while (firstHalf.length !== 0 && secondHalf.length !== 0) {
+    currentMin = findMinAndRemove(firstHalf, secondHalf);
+    sorted.push(currentMin);
+  }
+  return sorted.concat(firstHalf).concat(secondHalf);
+}
